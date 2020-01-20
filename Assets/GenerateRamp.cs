92eb195b75a9c61ca049d46f343fb77f7f2ab584
Jason Nelson <jason.andr.nelson@gmail.com>
2020-01-20 13:35:13 -0800

    updating script to adjust x and y more logarithmically, but the y part is still broken. going to just make a quarter circle instead.

--- Assets/GenerateRamp.cs
+++ Assets/GenerateRamp.cs
@@ -8,15 +8,27 @@ public class GenerateRamp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Transform initialRampTransform = initialRamp.GetComponent<Transform>();
+
+        //Debug.Log($"Initial x positon: {initialRampTransform.position.x}");
+        float xOffsetTotal = initialRampTransform.position.x;
         for (int i = 1; i < 90; i++)
         {
-            Transform initialRampTransform = initialRamp.GetComponent<Transform>();
-            float newXPosition = initialRampTransform.position.x - (i * initialRampTransform.localScale.x);
-            float newYPosition = initialRampTransform.position.y + i*.05f;
+            float currXRatio = ((90f-i)/90f);
+            //Debug.Log($"xratio: {currXRatio}");
+            float xOffset = currXRatio * initialRampTransform.localScale.x;
+            //Debug.Log($"xoffset: {xOffset}");
+            xOffsetTotal += xOffset;
+            float newXPosition = initialRampTransform.position.x - (xOffsetTotal);
+
+            float currYRatio = (i/90f);
+            Debug.Log($"yratio: {currYRatio}");
+            float yOffset = (i*i)*currYRatio;
+            Debug.Log($"yoffset, i/ratio: {yOffset}");
+            float newYPosition = initialRampTransform.position.y + yOffset;
             Debug.Log($"new x: {newXPosition}, new y: {newYPosition}");
             GameObject nextRampPart;
             nextRampPart = Instantiate(initialRamp, new Vector3(newXPosition, newYPosition, initialRampTransform.position.z), Quaternion.Euler(new Vector3(0,0,-1*i)),this.transform);
-            Debug.Log($"new x: {newXPosition}, new y: {newYPosition}");
 
             nextRampPart.name = $"ramp-at-degree-{i}";
         }
