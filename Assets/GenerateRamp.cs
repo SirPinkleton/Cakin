481483f58913180168f86c73e0a06b295c3bbb09
Jason Nelson <jason.andr.nelson@gmail.com>
2020-01-20 12:35:20 -0800

    my unity editor decided to change some asset and assembly files. no idea why, but I assume something is different about our environments. anyway, adding a script to generate a ramp off of a given game object, and adding it to the sample scene

---
+++ Assets/GenerateRamp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateRamp : MonoBehaviour
+{
+    [SerializeField] GameObject initialRamp;
+    [SerializeField] float xPositionAdjustment = -10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialRamp = gameObject;
+
+        for (int i = 1; i < 90; i++)
+        {
+            Transform initialRampTransform = initialRamp.GetComponent<Transform>();
+            float newXPosition = initialRampTransform.position.x + (i * xPositionAdjustment);
+            float newYPosition = initialRampTransform.position.y + i;
+
+            GameObject nextRampPart;
+            nextRampPart = Instantiate(initialRamp, new Vector3(newXPosition, newYPosition, initialRampTransform.position.z), new Quaternion(0,0,(-1*i),0));
+
+            nextRampPart.name = $"ramp-at-degree-{i}";
+        }
+    }
+
+}
