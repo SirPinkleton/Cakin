8ccdc5e3b2aa0e5aa096e84a49e8e65939de106d
Jason Nelson <jason.andr.nelson@gmail.com>
2020-01-20 16:29:49 -0800

    new script, applied to the 'dog' object: simply applies a force onto the player if they get too close. ideally this would be written to apply to more than just the player, I assume we can make use of labels/layers for this

---
+++ Assets/Deterrent_Effect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deterrent_Effect : MonoBehaviour
+{
+    [SerializeField] float deterrentStrength = 50;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log("#collision");
+        if (col.gameObject.name == "Player")
+        {
+            //trigger bark animation
+            Debug.Log($"#my position: {transform.position}");
+            Debug.Log($"#colliders position: {col.transform.position}");
+            Vector3 direction = transform.position - col.transform.position;
+            Debug.Log($"#move away direction: {direction}");
+            direction.Normalize();
+            Debug.Log($"#normalized: {direction}");
+            col.GetComponent<Rigidbody2D>().velocity = new Vector2(deterrentStrength, direction.y + 10);
+            Debug.Log($"#new velocity: {col.GetComponent<Rigidbody2D>().velocity}");
+        }
+    }
+}
