352a6a118b1ba970653f45bcabbdc23f5ea79421
Jason Nelson <jason.andr.nelson@gmail.com>
2020-01-25 10:33:44 -0800

    adding editor mode specific test suite. currently not much working here

---
+++ Assets/EditModeTests/TestSuite.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEditor.SceneManagement;
+
+namespace Tests
+{
+    public class TestSuite
+    {
+public class MovementTests
+    {
+        [UnityTest]
+        public IEnumerator CanJump()
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity");
+
+            new WaitForSeconds(1);
+
+            var player = GameObject.Find("Player");
+
+            Debug.Log(player);
+
+            //get initial place
+            var initialHeight = player.transform.position.y;
+
+            //press space
+            //requires interface...
+
+            //allow time to pass, for part of jump to happen
+            new WaitForSeconds(0.1f);
+
+            //player should be higher
+            Assert.True(player.transform.position.y > initialHeight);
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TurnSpeedTest()
+        {
+            yield return null;
+        }
+
+        [TearDown]
+        public void AfterEveryTest()
+        {
+            //foreach (var object in GameObject.Find(everything))
+            //{Object.Destroy(object)}
+        }
+    }
+    }
+}
