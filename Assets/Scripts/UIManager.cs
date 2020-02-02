3b101d468c51238aeef757794495b3f19e103d59
Jason Nelson <jason.andr.nelson@gmail.com>
2020-02-02 11:15:04 -0800

    adding UI manager, which other element can talk to when needed

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    string _ingredientText;
+    [SerializeField] Text _uiIngredientTextComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void UpdateIngredientUI(Dictionary<string, int> newInventory)
+    {
+        //responsiveness?? maybe set up via the textbox settings instead
+        string ingredients = string.Join(" ", newInventory.Select(i => $"{i.Key}: {i.Value}").ToList());
+        _uiIngredientTextComponent.text = ingredients;
+    }
+}
