2e084c0f796c496fb5cf61e0885f884c4e80a662
Jason Nelson <jason.andr.nelson@gmail.com>
2020-02-02 11:25:25 -0800

    decided I like the background, but with some transparenct

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,18 +8,21 @@ public class UIManager : MonoBehaviour
 {
     string _ingredientText;
     [SerializeField] Text _uiIngredientTextComponent;
+    [SerializeField] Image _uiIngredientBackgroundComponent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Color newBackColor = _uiIngredientBackgroundComponent.color;
+        newBackColor.a = 0.9f;
+        _uiIngredientBackgroundComponent.color = newBackColor;
     }
 
     // Update is called once per frame
     public void UpdateIngredientUI(Dictionary<string, int> newInventory)
     {
         //responsiveness?? maybe set up via the textbox settings instead
-        string ingredients = string.Join(" ", newInventory.Select(i => $"{i.Key}: {i.Value}").ToList());
+        string ingredients = string.Join("  ", newInventory.Select(i => $"{i.Key}: {i.Value}").ToList());
         _uiIngredientTextComponent.text = ingredients;
     }
 }
