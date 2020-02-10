3c636b96fa93fdaa09d9532acc810b7c2c7d3d2b
Jason Nelson <jason.andr.nelson@gmail.com>
2020-02-09 18:32:35 -0800

    currently, this script just supplies an action for the button. eventually, when we have souhnd effects and music, we'll add functions for defining what the sliders do.

---
+++ Assets/Scripts/Options.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour
+{
+    public GameObject panelToReturnToOnExit;
+
+    public void ExitOptions()
+    {
+        gameObject.SetActive(false);
+        panelToReturnToOnExit.SetActive(true);
+    }
+}
