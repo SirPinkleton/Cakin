04c8f2567dca9016fac14210f833744409722003
Jason Nelson <jason.andr.nelson@gmail.com>
2020-02-09 18:30:19 -0800

    adding main menu. It's a simple panel, title +4 buttons. new game (starts sample scene), continue (currently undefined, requires save file), options (shows options prefab), and quit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionsUI;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Continue()
+    {
+        Debug.Log("Need to save currently played level to a file, read in here to load it again? Open a new panel showing all of the levels to play?");
+    }
+
+    public void Options()
+    {
+        gameObject.SetActive(false);
+        optionsUI.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
